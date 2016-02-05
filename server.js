797545b05ea3a3b675d6f40cc7f3aa39b47fae89
Sam Vipond <sammy.potato@gmail.com>
2016-02-05 17:53:04 -0500

    Add DELETE todos route

--- server.js
+++ server.js
@@ -53,6 +53,20 @@ app.post('/todos', function (req, res) {
 	res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var match = _.findWhere(todos, {id: todoId});
+
+	if(!match){
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, match);
+		console.log(match);
+		res.json(match);
+	}
+});
+
 
 
 
